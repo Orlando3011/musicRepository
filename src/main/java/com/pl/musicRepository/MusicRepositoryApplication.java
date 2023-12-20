57d831875cc5c7bfe9e50199e41fb4eff1bb0070
Orlando3011 <mwasik95@gmail.com>
2023-12-20 07:43:43 +0100

    Initial commit with Spring Initializr

---
+++ src/main/java/com/pl/musicRepository/MusicRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.pl.musicRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pl/musicRepository/MusicRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.musicRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
